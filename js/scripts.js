fe6b8eeca85d0e37fc0076fc144b69e659b8e89b
Sandra Castaneda <sancastans@gmail.com>
2018-11-02 16:00:33 -0700

    added some functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
+//Builds a Pizza Logic
+
+function Pizza (sauce, toppings, size){
+  this.sauce = sauce
+  this.toppings = toppings
+  this.size = size
+}
 
 
 $(document).ready(function() {
-  $("form#suggester").submit(function(event) {
+  $("form#pizza").submit(function(event) {
     event.preventDefault();
-
-
-    var q1 = $("select#q1").val();
-    var q2 = $("select#q2").val();
-    var q3 = $("select#q3").val();
-    var q4 = $("select#q4").val();
-    var q5 = $("select#q5").val();
-
-    if (q1==="pro" && q2==="large" && q3==="software" && q4==="databases" && q5==="hammer"){
-      $("#c").show();
-    } else if (q1==="skill" && q2==="large" && q3==="interfaces" && q4==="fundamentals" && q5==="sword"){
-          $("#java").show();
-    } else if (q1==="fun" && q2==="small" && q3==="apps" && q4==="simplicity" && q5==="axe"){
-          $("#ruby").show();
-    }
-  });
+var pizza = new Pizza (sauce, toppings, size)
+$("#total").show();
+ $("input:checkbox[name=toppings]:checked").each(function(){
+   var top = $(this).val();
+   console.log(top);
+});
+   $("#total").show();
+    $("input:checkbox[name=sauce]:checked").each(function(){
+      var top = $(this).val();
+    });
+    });
 });
